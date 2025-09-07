908664ba2744223cc6d2518c82c8f29e29f30eb6
BM Loukas <loukas@brickmate.vn>
2025-09-07 22:35:03 +0700

    refactor (App) : Remove sourceMaps

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,12 +1,11 @@
-import { defineConfig } from "tsup";
+import { defineConfig } from "tsup"
 
 export default defineConfig({
-    entry: ["src/index.ts"],
-    format: ["cjs", "esm"], // Build for commonJS and ESmodules
-    dts: true, // Generate declaration file (.d.ts)
-    splitting: false,
-    sourcemap: true,
-    clean: true,
-    minify: true,
-    outDir: 'dist', // Output directory
-});
+  entry: ["src/index.ts"],
+  format: ["cjs", "esm"], // Build for commonJS and ESmodules
+  dts: true, // Generate declaration file (.d.ts)
+  sourcemap: false,
+  clean: true,
+  minify: true,
+  outDir: "dist", // Output directory
+})
